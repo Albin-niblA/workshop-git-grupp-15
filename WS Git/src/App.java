9bb9067de7ebc716034bc634fd75b1469ed46ffd
Albin <albint252@gmail.com>
2026-03-26 10:12:50 +0100

    Update app.java
    Read operand b

--- WS Git/src/App.java
+++ WS Git/src/App.java
@@ -6,5 +6,6 @@ public class App {
         if("1".equals(choice) System.out.println(Calculator.add(0, 0));
         System.out.println("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit");
         if("1".equals(choice)) System.out.println("TODO Add");
+        double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
     }
 }
